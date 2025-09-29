cfb380acdedcb5fe3b422977ef1a794abd7ec946
diasdasilvajoaovitor673-ship-it <diasdasilvajoaovitor673@gmail.com>
2025-09-28 21:04:07 -0400

    Constants.py

--- atividade-005/breakout.py
+++ atividade-005/breakout.py
@@ -1,3 +1,20 @@
 import pygame
 import random
 import os
+
+# Game Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+BALL_RADIUS = 10
+BALL_SPEED_X = 5
+BALL_SPEED_Y = 5
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 20
+PADDLE_SPEED = 7
+FPS = 60
