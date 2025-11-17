fbdd3f790a4237403c54ff089feb8927d961960d
Adinan <adinanmatosjunior021@gmail.com>
2025-11-17 17:42:24 -0400

    atualização do arquivo python sounds

--- atividade-009/src/sounds.py
+++ atividade-009/src/sounds.py
@@ -9,3 +9,5 @@ pygame.mixer.init()
 # Sounds
 BASE = Path(__file__).resolve().parent
 sound_shot = pygame.mixer.Sound("atividade-009/sounds/fire.wav")
+break_small = pygame.mixer.Sound("atividade-009/sounds/bagSmall.wav")
+break_medium = pygame.mixer.Sound("atividade-009/sounds/bagMedium.wav")
