013d1f9a0b35dd87a45ab1b80fbb8e999fe5e7c3
Adinan <adinanmatosjunior021@gmail.com>
2025-11-23 23:45:10 -0400

    atualização dos sons

--- atividade-009/src/sounds.py
+++ atividade-009/src/sounds.py
@@ -8,6 +8,6 @@ pygame.mixer.init()
 
 # Sounds
 BASE = Path(__file__).resolve().parent
-sound_shot = pygame.mixer.Sound("atividade-009/sounds/fire.wav")
-break_small = pygame.mixer.Sound("atividade-009/sounds/bagSmall.wav")
-break_medium = pygame.mixer.Sound("atividade-009/sounds/bagMedium.wav")
+sound_shot = pygame.mixer.Sound("C:\\Users\\adina\\OneDrive\\Documents\\lpc2025\\lpc2025\\atividade-009\\sounds\\fire.wav")
+break_small = pygame.mixer.Sound("C:\\Users\\adina\\OneDrive\\Documents\\lpc2025\\lpc2025\\atividade-009\\sounds\\bangSmall.wav")
+break_medium = pygame.mixer.Sound("C:\\Users\\adina\\OneDrive\\Documents\\lpc2025\\lpc2025\\atividade-009\\sounds\\fire.wav")
